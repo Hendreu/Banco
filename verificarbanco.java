ebb1473cc04b8fc6d87345a182793504c8967056
Hendreu <89034425+Hendreu@users.noreply.github.com>
2024-04-22 12:41:54 -0300

    Add files via upload

---
+++ verificarbanco.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class verificarbanco {
+    public static void main(String[] args) {
+
+        Scanner dados = new Scanner(System.in);
+        Scanner dinheiro = new Scanner(System.in);
+        int botoes = 0;
+
+        Boolean Cliente = true;
+        String NomeDoUsuario = "Hendreu";
+        String TipoDeConta = "Saque";
+        double DinheiroNaConta = 0;
+        double Receber = 0;
+        double Transferencia = 0;
+
+        while (Cliente = true && botoes !=4){
+            System.out.println(
+            " -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-" +
+            "\nNome:" + NomeDoUsuario+
+            "\nTipo de Conta:" + TipoDeConta +
+            "\nDinheiro na conta:" + DinheiroNaConta +
+            "\n -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-" +
+            "\n\n 1.Verificar saldo da conta" +
+            "\n 2.Receber Valor" +
+            "\n 3.Transferir Valor" +
+            "\n 4.Extrato bancario"
+            );
+
+            botoes =  dados.nextInt();
+
+         if (botoes == 1) {
+                System.out.println("Saldo da conta:" + DinheiroNaConta);
+        } else if (botoes == 2){
+                System.out.println("Valor a se receber");
+                Receber = dinheiro.nextDouble();
+                DinheiroNaConta += Receber;
+                System.out.println("Seu novo saldo é de:"+ DinheiroNaConta);
+        } else if (botoes == 3){
+                System.out.println("Valor a se transferir");
+                Transferencia = dinheiro.nextDouble();
+                DinheiroNaConta -= Transferencia;
+                System.out.println("Seu novo saldo é de:"+ DinheiroNaConta);
+        } else{
+                System.out.println("Obrigado por sua operação");
+
+                }
+            }
+        }
+    }
+    /* switch (botoes){
+            case 1: System.out.println("Saldo da conta:" + DinheiroNaConta); break;
+
+            case 2:System.out.println("Valor a se receber");
+                Receber = dinheiro.nextDouble();
+                DinheiroNaConta += Receber;
+                System.out.println("Seu novo saldo é de:"+ DinheiroNaConta); break;
+            case 3:System.out.println("Valor a se transferir");
+                Transferencia = dinheiro.nextDouble();
+                DinheiroNaConta -= Transferencia;
+                System.out.println("Seu novo saldo é de:"+ DinheiroNaConta); break;
+            case 4: System.out.println("Obrigado por sua operação"); break;
+            */
